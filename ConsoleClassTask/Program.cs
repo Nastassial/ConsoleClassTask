424c7550799b36ed4c8d3dd37877d416c7198484
Nastassial <nastassialmezhen@gmail.com>
2023-10-17 19:56:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleClassTask/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection;
+
+List<Phone> phones = new List<Phone>() 
+{ 
+    new Phone("+375291234567","Apple", 11.1), 
+    new Phone("+375299876543", "Samsung", 22.2), 
+    new Phone("+375295437861", "Xiaomi", 33.3) 
+};
+
+foreach (Phone phone in phones)
+{
+    Console.WriteLine("Информация о телефоне:");
+    Console.WriteLine($"Номер телефона - {phone.Number}, модель - {phone.Model}, вес - {phone.Weight}");
+    phone.ReceiveCall("Неизвестный абонент");
+}
+
+Console.WriteLine($"Номер этого телефона {phones[0].GetNumber()}");
+phones[1].ReceiveCall("кто-то", "+375291111111");
+phones[2].SendMessage("+375291231212", "+375292342323", "+375293453434");
+phones[2].SendMessage();
+phones[2].SendMessage(new string[] { "+375291231212", "+375292342323", "+375293453434" });
+
+public class Phone
+{
+    public string Number { get; }
+    public string Model { get; }
+    public double Weight { get; }
+
+    public Phone() 
+    { 
+        Number = "+375290000000";
+        Model = "неизвестно";
+        Weight = 0.0;
+    }
+
+    public Phone(string number, string model)
+    {
+        Number = number;
+        Model = model;
+    }
+
+    public Phone(string number, string model, double weight) : this(number, model)
+    {
+        Weight = weight;
+    }
+
+    public void ReceiveCall(string name) => Console.WriteLine($"Звонит {name}");
+
+    public void ReceiveCall(string name, string number) => Console.WriteLine($"Звонит {name} Номер {number}");
+
+    public string GetNumber() => Number ?? "нет номера"; 
+
+    public void SendMessage(params string[] numbers)
+    {
+        Console.WriteLine("Список номеров:");
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ ConsoleClassTask2/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+Каждый врач имеет метод «лечить», но каждый врач лечит по-своему.
+*/
+
+List<Doctor> doctors = new List<Doctor>()
+{
+    new Doctor("Вася", "хирург"),
+    new Doctor("Петя", "терапевт"),
+    new Doctor("Катя", "дантист")
+};
+
+List<Patient> patients = new List<Patient>()
+{
+    new Patient("Маша"),
+    new Patient("Света", new CurePlan("1")),
+    new Patient("Игорь")
+};
+
+public class Person
+{
+    public string Name { get; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Doctor : Person
+{
+    public string? Post { get; }
+
+    public Doctor(string name) : base(name)
+    { }
+    public Doctor(string name, string post): base(name) 
+    {
+        Post = post;
+    }
+
+    public void Cure() { }
+}
+
+public class Patient : Person
+{
+    public CurePlan? Plan { get; set; }
+
+    public Doctor? Doctor { get; set; }
+    public Patient(string name) : base(name) { }
+    public Patient(string name, CurePlan plan) : this(name) 
+    {
+        Plan = plan;
+    }
+    public Patient(string name, CurePlan plan, Doctor doctor) : this(name, plan)
+    {
+        Doctor = doctor;
+    }
+
+    public void SetDoctor()
+    {
+        Doctor = Plan.Description switch
+        {
+            "1" => new Doctor("Вася", "хирург"),
+            "2" => new Doctor("Вася", "дантист"),
+            _ => new Doctor("Катя", "терапевт")
+        };
+
+        Doctor.Cure();
+    }
+}
+
+public class CurePlan
+{
+    public string Description { get; set; }
+
+    public CurePlan(string description)
+    {
+        Description = description;
+    }
+}
